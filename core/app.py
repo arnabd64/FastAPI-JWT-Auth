010b44ccc4005aec02a53700f6c0eab5618b6adb
arnabd64 <arnabdhar32@gmail.com>
2024-09-06 15:14:11 +0530

    //WIP

--- core/app.py
+++ core/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
-import auth.routes
+import auth
 
 app = FastAPI(
     title = "FastAPI JWT Authentication"
 )
 
-app.include_router(auth.routes.router)
+app.include_router(auth.router)
 
 @app.get('/', response_class=PlainTextResponse)
 async def root():
